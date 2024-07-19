66fc67b5dd70443d22cbd384fb942b83ceb211aa
Alex Reyes <73624939+AlexReyesHuezo@users.noreply.github.com>
2024-07-19 17:26:52 -0600

    route the character controller nowdays implemented methods

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CharacterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/characters', [CharacterController::class, 'index'])->name('characters.index');
+Route::get('/characters/create', [CharacterController::class, 'create'])->name('characters.create');
+Route::post('/characters', [CharacterController::class, 'store'])->name('characters.store');
+Route::get('/characters/{id}', [CharacterController::class, 'show'])->name('characters.show');
+Route::put('/character/{is}', [CharacterController::class, 'edit'])->name('characters.edit');
+
+?>
